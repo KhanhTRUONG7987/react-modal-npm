2c4857f52965a7e81cdf4db0551c2ae72e3eaae0
khanh.truong <abby79truong@gmail.com>
2024-03-18 10:07:13 +0100

    update

--- react-modal-project/src/lib/index.js
+++ react-modal-project/src/lib/index.js
@@ -1,9 +1,9 @@
 // Import all components
-import Modal from './Modal/Modal';
-import { ModalProvider, useModal } from './ModalContext/ModalContext';
-import ModalManager from './ModalManager/ModalManager';
-import ModalTrigger from './ModalTrigger/ModalTrigger';
-import Overlay from './Overlay/Overlay';
+import Modal from './Modal/Modal.jsx';
+import { ModalProvider, useModal } from './ModalContext/ModalContext.jsx';
+import ModalManager from './ModalManager/ModalManager.jsx';
+import ModalTrigger from './ModalTrigger/ModalTrigger.jsx';
+import Overlay from './Overlay/Overlay.jsx';
 
 // Export all components
 export { ModalProvider, useModal, Modal, ModalManager, ModalTrigger, Overlay };
